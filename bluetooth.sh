7b3b747780e2b76ff91e6c0531118695d79155d6
Petr Sedláček <=>
2019-02-21 18:27:14 +0100

    Update bluetooth for arch

--- bluetooth.sh
+++ bluetooth.sh
@@ -9,6 +9,6 @@ choices="Connect
 Disconnect"
 chosen=$(echo "$choices" | dmenu -i)
 case "$chosen" in
-    Connect) bluetoothctl <<< 'connect 70:26:05:55:B2:0F' ;;
+    Connect) blueman-applet & sleep 2 && bluetoothctl <<< 'connect 70:26:05:55:B2:0F' && sleep 5 && killall blueman-applet ;;
     Disconnect) bluetoothctl <<< 'disconnect 70:26:05:55:B2:0F' ;;
 esac
